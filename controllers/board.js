58d651c14cc55c7c92a50d62309cec87a44afa6e
Brian Crescimanno <brian.crescimanno@gmail.com>
2014-01-27 22:51:17 -0800

    Grab the lists and cards up front--honestly probably a bad idea in the long run but it works for now

--- controllers/board.js
+++ controllers/board.js
@@ -2,6 +2,7 @@
 
 var Trello = require("node-trello"),
     nconf = require("nconf"),
+    async = require("async"),
     Board = require('../models/board');
 
 module.exports = function (app) {
@@ -9,23 +10,41 @@ module.exports = function (app) {
     var key = nconf.get("trelloKey"),
         trello = new Trello(key),
         urlPattern = '/1/boards/',
-        boardId,
+        generateAsyncTrelloRequest,
         model,
+        listsUrl,
+        cardsUrl,
         url;
+
+    generateAsyncTrelloRequest = function (url) {
+        return function (callback) {
+            trello.get(url, function (err, data) {
+                callback(err, data);
+            });
+        }
+    };
 
     app.get('/board/:id', function (req, res) {
 
         url = urlPattern + req.params.id;
-        trello.get(url, function (err, data) {
+        listsUrl = url + "/lists";
+        cardsUrl = url + "/cards";
+
+        async.parallel({
+            board: generateAsyncTrelloRequest(url),
+            lists: generateAsyncTrelloRequest(listsUrl),
+            cards: generateAsyncTrelloRequest(cardsUrl)
+        }, function (err, data) {
             if (!err) {
-                model = new Board(data);
-                console.log(data);
+                model = new Board(data.board);
+                model.numLists = data.lists.length;
+                model.numCards = data.cards.length;
                 res.render('board', model);
             } else {
                 // This is placeholder shit
                 res.render('index', {name: 'Super Fail Time!'})
             }
-        });
+        })
     });
 
 };
